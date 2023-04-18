02d2191ed59ecb4b67768bc7d87d124e0a2121e0
AirsayOS <eseigbeebhota@gmail.com>
2023-04-18 21:15:38 +0100

    Optimisation

--- main.c
+++ main.c
@@ -3,10 +3,74 @@
 #include <stdbool.h>
 #include <string.h>
 
+bool is_valid_operand(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+bool parse_input(char* input, double* num1, double* num2, char* operand) {
+    char* endptr;
+
+    // parse first number
+    *num1 = strtod(input, &endptr);
+
+    // check if input is valid
+    if (endptr == input) {
+        printf("Invalid input. Please try again.\n");
+        return false;
+    }
+
+    // skip any white spaces
+    while (*endptr == ' ') {
+        endptr++;
+    }
+
+    // check if operand is valid
+    if (!is_valid_operand(*endptr)) {
+        printf("Invalid input. Please try again.\n");
+        return false;
+    }
+    *operand = *endptr;
+
+    // use strtod to parse second number
+    *num2 = strtod(endptr+1, &endptr);
+
+    // check if input is valid
+    if (endptr == input || *endptr != '\n') {
+        printf("Invalid input. Please try again.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void perform_operation(double num1, double num2, char operand) {
+    switch (operand) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error: division by zero\n");
+            } else {
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            }
+            break;
+        default:
+            printf("Invalid operand. Please try again.\n");
+            break;
+    }
+}
+
 int main() {
-    char *input = NULL;
-    char operand;
+    char* input = NULL;
     double num1, num2;
+    char operand;
     bool valid_input = false;
 
     while (!valid_input) {
@@ -22,74 +86,15 @@ int main() {
         // read input string from stdin
         fgets(input, 100, stdin);
 
-        // use strtod to convert input string to double
-        char *endptr;
-        num1 = strtod(input, &endptr);
+        // parse input
+        valid_input = parse_input(input, &num1, &num2, &operand);
 
-        // check if input is valid
-        if (endptr == input) {
-            printf("Invalid input. Please try again.\n");
-            free(input);
-            continue;
-        }
-
-        // skip any white spaces
-        while (*endptr == ' ') {
-            endptr++;
-        }
-
-        // check if operand is valid
-        operand = *endptr;
-        if (operand == '\0' || operand == '\n') {
-            printf("Invalid input. Please try again.\n");
-            free(input);
-            continue;
-        }
-
-        // use strtod to convert second number
-        num2 = strtod(endptr+1, &endptr);
-
-        // check if input is valid
-        if (endptr == input || *endptr != '\n') {
-            printf("Invalid input. Please try again.\n");
-            free(input);
-            continue;
-        }
-
-        // perform arithmetic operation based on operand
-        switch (operand) {
-            case '+':
-                printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
-                valid_input = true;
-                break;
-
-            case '-':
-                printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
-                valid_input = true;
-                break;
-
-            case '*':
-                printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
-                valid_input = true;
-                break;
-
-            case '/':
-                if (num2 == 0) {
-                    printf("Error: division by zero\n");
-                } else {
-                    printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
-                }
-                valid_input = true;
-                break;
-
-            default:
-                printf("Invalid operand. Please try again.\n");
-                free(input);
-                break;
-        }
+        // free memory
+        free(input);
     }
 
-    //free(input);
+    // perform arithmetic operation based on operand
+    perform_operation(num1, num2, operand);
 
     return 0;
 }
